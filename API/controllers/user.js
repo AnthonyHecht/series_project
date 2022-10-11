260350539bf505518a43112cfcd00de57df855cc
Anthony <kikooleroxxor@gmail.com>
2022-10-11 13:10:16 +0200

    structure back

---
+++ API/controllers/user.js
@@ -0,0 +1,57 @@
+const User = require("../models/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+exports.login = (req, res, next) => {
+  
+  
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ email: "Utilisateur non trouvé !" });
+      }
+      bcrypt
+        .compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res
+              .status(401)
+              .json({ password: "Mot de passe incorrect !" });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: jwt.sign({ 
+              userId: user._id,
+              role: user.role,
+
+             }, process.env.JWT_KEY),
+          });
+        })
+        .catch((err) => res.status(500).json({ error: err }));
+    })
+    .catch((err) => res.status(500).json({ error: err }));
+};
+
+exports.logout = (req, res, next) => {
+  const removeToken = sessionStorage.removeItem("token");
+  const removeUserId = sessionStorage.removeItem("userId");
+  if (removeToken && removeUserId) {
+    res.status(200).json({ message: "Déconnexion réussie !" });
+  } else {
+    res.status(401).json({ message: "Vous n'êtes pas connecté !" });
+  }
+};
+
+exports.getUser = (req, res, next) => {
+  User.findById(req.params.id)
+    .select("-password")
+    .then((user) => res.status(200).json(user))
+    .catch((err) => res.status(500).json({ error: err }));
+};
+
+exports.getAllUsers = (req, res, next) => {
+  User.find()
+    .select("-password")
+    .then((users) => res.status(200).json(users))
+    .catch((err) => res.status(500).json({ error: err }));
+};
---
+++ API/router/user.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+
+const userCtrl = require("../controllers/user");
+const auth = require("../middleware/auth");
+
+//login logout routes
+router.post("/login", userCtrl.login);
+router.get("/logout", userCtrl.logout);
+
+// User routes
+router.get("/", auth, userCtrl.getAllUsers);
+router.get("/:id", auth, userCtrl.getUser);
+
+module.exports = router;
