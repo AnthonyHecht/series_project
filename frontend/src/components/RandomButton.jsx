e857c7a8bbecfd488ad9a37946cc1fc06b2b9195
Anthony <kikooleroxxor@gmail.com>
2023-02-07 17:01:03 +0100

    random serie

--- frontend/src/components/RandomButton.jsx
+++ frontend/src/components/RandomButton.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { CardList } from "../datas/CardList";
 
 const RandomButton = () => {
-    const random = CardList[Math.floor(Math.random() * CardList.length)]
+    
+        function handleClick(e) {
+            e.preventDefault();
+            const nameList = CardList.map(item => item.name)
+            const random = nameList[Math.floor(Math.random() * nameList.length)]
+            alert(random)
+        }
     return (
-        <button className="randomButton" onClick={random}>?</button>
+        <button className="randomButton" onClick={handleClick}>?</button>
     )
 }
 
